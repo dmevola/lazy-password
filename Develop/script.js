7f6fb8b15719eff2afa19754f95dd8b08dfeeba4
dmevola <dmevola@gmail.com>
2021-12-18 10:18:45 -0500

    framed out pseudo code and initial prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,36 @@
 // Assignment code here
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyx";
+var numbers = "0123456789";
+var specialChar = "!@#$%^&*()?/;:~";
 
+// user selection and final generated password
+var userSelection = [];
+var finalPassword = [];
+
+// generate password function
+
+function generatePassword() {
+
+  // password length prompt
+
+  var passLength = prompt("Please enter a number between 8 and 128");
+  if (passLength < 8) {
+    alert ("Please enter a number between 8 and 128");
+    generatePassword();
+  }
+  if (passLength > 128) {
+    alert ("Please enter a number between 8 and 128");
+    generatePassword();
+  }
+
+  // uppercase prompt
+var upperC = confirm("Would you lke Uppercase Letters?");
+if (upperC) {
+  userSelection.push(upperCase);
+}
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +46,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
